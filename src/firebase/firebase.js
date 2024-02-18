ecc0fee48be5fbd1144c79e5c81edc0ecc95132f
nishii118 <poro2420033@gmail.com>
2024-02-18 23:58:36 +0700

    update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,19 +4,19 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAdoHRFYap_ps-DQbZ1mmWZD7QL6KI_mGE",
-  authDomain: "insta-clone-4da1b.firebaseapp.com",
-  projectId: "insta-clone-4da1b",
-  storageBucket: "insta-clone-4da1b.appspot.com",
-  messagingSenderId: "507181473504",
-  appId: "1:507181473504:web:48350bcb5861a5b14fba85",
-  measurementId: "G-C2B8GWFDMN"
+	apiKey: "AIzaSyBF7nW_w7zyFenHrVBXUc-vNw0kZYcrrIc",
+  authDomain: "insta-clone2-d4119.firebaseapp.com",
+  projectId: "insta-clone2-d4119",
+  storageBucket: "insta-clone2-d4119.appspot.com",
+  messagingSenderId: "1009415397231",
+  appId: "1:1009415397231:web:c21510f110d85e2ee1929a",
+  measurementId: "G-F0WR27YDQY"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
-
+// firebase.firestore.enablePersistence();
 export { app, auth, firestore, storage };
 
